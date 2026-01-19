3c3853b3ed8bc476af4b8fbc58acc2cbb519a27b
nishant.kohale <nishant.kohale@click2cloud.net>
2026-01-19 13:00:53 +0530

    Nishant :- Enhance chat functionality with audio playback controls and environment configuration

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  googleApiKey: 'As',      
+  pythonBackendURL: 'https://your-production-domain:9015/',
+  enableDebugMode: false
+};
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: false,
+  googleApiKey: 'As',
+  pythonBackendURL: 'https://your-production-domain:9015/',
+  enableDebugMode: true
+};
